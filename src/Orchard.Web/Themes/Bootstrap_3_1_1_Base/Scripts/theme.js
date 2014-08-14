e6bb4c333cf574ca13c1126e0d04dbebec81027e
Shaun Luttin <shaun@bigfont.ca>
2014-08-13 22:34:34 -0700

    Tweak the css and js.

--- src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
+++ src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
@@ -2,7 +2,7 @@
     // todo - Put this into the child theme
 
     $(window).resize(function () {
-        var masthead = $("#layout-header");
+        var masthead = $(".widget-masthead");
         var width = masthead.width();                
         var height = width * 700 / 1200;
         masthead.height(height);
