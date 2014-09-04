941711d76ce8dd159729db436d7dbcd2090e21b1
Shaun Luttin <shaunluttin@gmail.com>
2014-09-04 13:04:43 -0700

    Update theme.js.

--- src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
+++ src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
@@ -2,7 +2,7 @@
     // todo - Put this into the child theme
 
     $(window).resize(function () {
-        var masthead = $(".widget-masthead");
+        var masthead = $(".widget-masthead-widget");
         var width = masthead.width();                
         var height = width * 700 / 1200;
         masthead.height(height);
