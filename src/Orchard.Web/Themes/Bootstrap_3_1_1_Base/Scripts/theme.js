d2b40e10da554c1be2c08e4f0fd1a5cb17813bd8
Shaun Luttin <shaun@bigfont.ca>
2014-09-04 19:41:43 -0700

    Add the script to the theme.

--- src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
+++ src/Orchard.Web/Themes/Bootstrap_3_1_1_Base/Scripts/theme.js
@@ -1,12 +1,1 @@
-﻿$(function () {
-    // todo - Put this into the child theme
-
-    $(window).resize(function () {
-        var masthead = $(".widget-masthead-widget");
-        var width = masthead.width();                
-        var height = width * 700 / 1200;
-        masthead.height(height);
-    });
-
-    $(window).resize();
-});
+﻿
